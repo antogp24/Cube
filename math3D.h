6ee92b4334549a779c254a843795b300c6947732
antogp <88955960+antogp24@users.noreply.github.com>
2022-10-01 10:30:25 -0500

    Delete math3D.h

--- math3D.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "math3D/frametimer.hpp"
-#include "math3D/matrix.hpp"
-#include "math3D/window.hpp"
-#include "math3D/shapes.hpp"
-#include "math3D/matrices.hpp"
-#include "math3D/vector.hpp"
